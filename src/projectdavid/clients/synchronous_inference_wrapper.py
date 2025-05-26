2e2c2bf1300ff5b31c43b64d6895e25658766d95
bmxitalia <tommasocarraro96@gmail.com>
2025-05-26 12:21:43 +0200

    added more information regarding exceptions

--- src/projectdavid/clients/synchronous_inference_wrapper.py
+++ src/projectdavid/clients/synchronous_inference_wrapper.py
@@ -89,7 +89,7 @@ class SynchronousInferenceStream:
                 break
             except Exception as e:
                 logging_utility.error(
-                    "Unexpected error during streaming completions: %s", e
+                    "Unexpected error during streaming completions: %s", e if e else repr(e)
                 )
                 break
 
